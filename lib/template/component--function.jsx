2148bb2a6a71011d695407ba3553ca870d9d10b9
minung.han <hmu332233@gmail.com>
2018-07-05 17:40:16 +0900

    [Add] function type template

---
+++ lib/template/component--function.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './[COMPONENT_NAME].scss';
+
+const [COMPONENT_NAME] = props => {
+  return (
+  );
+}
+
+[COMPONENT_NAME].propTypes = {
+};
+[COMPONENT_NAME].defaultProps = {
+};
+
+export default [COMPONENT_NAME];
